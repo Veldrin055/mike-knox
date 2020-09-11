c6d207bab0d1537bc4a5f74fed5e229a94d99e3b
Daniel Morrison <daniel.morrison@oramon.nl>
2020-09-11 16:24:39 +1000

    CSS fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-full bottom-0 flex flex-col bg-gray-900 text-gray-500 text-sm fixed z-20">
+  <footer class="w-full bottom-0 flex flex-col bg-gray-900 text-gray-500 text-xs md:text-sm fixed z-20">
     <span>Authorised by Tomas Cifuentes, 2 Francis Cres, Ferntree Gully</span>
     <span>Copyright 2020 Michael Koclega</span>
     <!-- <font-awesome-icon :icon="['fab', 'instagram']" /> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="h-full purple-grad">
   <div class="container mx-auto px-6 flex flex-col items-center justify-center">
-  <div class="fixed overflow-hidden z-0 pos">
-    <font-awesome-icon class="spinny opacity-75" :icon="['fas', 'bahai']" />
+  <div class="fixed z-0 pos">
+    <font-awesome-icon class="spinny" :icon="['fas', 'bahai']" />
   </div>
-    <div class="my-6 w-1/3 z-10">
+    <div class="my-6 w-1/2 md:w-1/3 z-10">
       <img class="object-contain shadow-lg" src="/assets/img/primary.jpg" />
     </div>
-    <div class="mx-2 mt-3 md:mt-12 w-1/2 text-center border-b-2 border-t-2 border-purple-300">
+    <div class="mx-2 mt-2 md:mt-12 md:w-1/2 text-center border-b-2 border-t-2 border-purple-300 z-10">
       <h1 class="text-gray-100 font-extrabold text-4xl xs:text-5xl md:text-6xl">
         Mike
       </h1>
-      <h2 class="text-gray-200 text-xl md:text-5xl leading-tight mt-4 md:mt-12">
+      <h2 class="text-gray-200 text-xl md:text-5xl leading-tight mt-2 md:mt-8 mb-2">
       The Wise Choice for Knox
       </h2>
     </div>
@@ -27,9 +27,8 @@
 .spinny {
   animation: spinner 40s linear infinite;
   font-size: 2000px;
-  opacity: 5%;
+  opacity: 0.05;
   z-index: 0;
-  overflow: hidden;
 }
 
 .pos {
