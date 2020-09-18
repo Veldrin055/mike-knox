f990215e8d7a00ff6d7cb5c28a53a60a07170d78
Daniel Morrison <daniel.morrison@oramon.nl>
2020-09-18 14:59:26 +1000

    fix about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,24 +6,11 @@
     </h3>
   </div>
   <div class="w-full text-justify text-gray-700 py-6 px-4 md:text-lg">
-    <p>I’m a 36 year old electronic music artist, data analytics consultant and former logistics officer in the Australian Army Reserve living in Ferntree Gully 10 minutes walk from the village.</p>
-    <p class="mt-6">My first contact with Ferntree Gully was in my childhood spending time at friends during the school holidays, but eventually I moved to Ferntree Gully when my father bought our family house here in 2000 before his untimely demise with cancer. I’ve lived here since I was 16 and through out my time at university studying computer science  at RMIT. After my studies I moved away and even spent 9 years living outside of Australia, split between Amsterdam and Kuala Lumpur and extensively traveling around the world, but Ferntree Gully always remained the family home with my mum holding down the fort.</p>
-    <p class="mt-6">I decided to move back home in late 2019 when visiting Melbourne for an extended period to help my mum out during her breast cancer treatment and recovery. I came to the conclusion from living overseas and my many travels, that Ferntree Gully and the surrounding area is one of the best places to live in the world with our unique blend of modern amenities and services and connection to raw nature with the Dandenongs National Park. I’d like to think that we can keep this place special and I’d want to do my part to help ensure it stays that way for future generations.</p>
-    <p class="mt-6">I don’t know everything, but I know enough to find and listen to those that know what they are talking about, and we have many people like this in our community. I want to talk with as many people as possible in the Dobson area community, but more importantly to listen, empathise and take action on your behalf. Vote for me as the next councillor for Dobson in the City of Knox council.</p>
-<div>
-  <div class="container m-auto px-6 p-10 flex flex-wrap">
-    <div class="mx-2 text-left border-b-2 border-purple-300">
-      <h3 class="text-gray-600 font-extrabold text-4xl xs:text-5xl md:text-6xl">
-        About me
-      </h3>
-    </div>
-    <div class="w-full text-justify text-gray-700 py-6 px-4">
-      <p>I’m a creative, empathic, analytical and resilient 36-year-old electronic music artist, freelance data engineer, and a former logistics officer in the Army Reserve.
+    <p>I’m a creative, empathic, analytical and resilient 36-year-old electronic music artist, freelance data engineer, and a former logistics officer in the Army Reserve.
 Last December I returned home, having lived abroad in Amsterdam and Kuala Lumpur for nine years. These experiences exposed me to unique and diverse lifestyles and perspectives not found in Australia. My time away from home has instilled profound gratitude for our unique foothills environment and lifestyle, and sense of community found in Ferntree Gully, which has been my family home for 20 years.
 I want to listen to the needs of the community with empathy and provide a way forward that supports the community, nurtures the environment and ensures wellbeing.</p>
-      <p class="mt-6">I decided to move back home in late 2019 when visiting Melbourne for an extended period to help my mum out during her breast cancer treatment and recovery. I came to the conclusion from living overseas and my many travels, that Ferntree Gully and the surrounding area is one of the best places to live in the world with our unique blend of modern amenities and services and connection to raw nature with the Dandenongs National Park. I’d like to think that we can keep this place special and I’d want to do my part to help ensure it stays that way for future generations.</p>
-      <p class="mt-6">I don’t know everything, but I know enough to find and listen to those that know what they are talking about, and we have many people like this in our community. I want to talk with as many people as possible in the Dobson area community, but more importantly to listen, empathise and take action on your behalf. Vote for me as the next councillor for Dobson in the City of Knox council.</p>
-    </div>
+    <p class="mt-6">I decided to move back home in late 2019 when visiting Melbourne for an extended period to help my mum out during her breast cancer treatment and recovery. I came to the conclusion from living overseas and my many travels, that Ferntree Gully and the surrounding area is one of the best places to live in the world with our unique blend of modern amenities and services and connection to raw nature with the Dandenongs National Park. I’d like to think that we can keep this place special and I’d want to do my part to help ensure it stays that way for future generations.</p>
+    <p class="mt-6">I don’t know everything, but I know enough to find and listen to those that know what they are talking about, and we have many people like this in our community. I want to talk with as many people as possible in the Dobson area community, but more importantly to listen, empathise and take action on your behalf. Vote for me as the next councillor for Dobson in the City of Knox council.</p>
   </div>
 </div>
 </template>
