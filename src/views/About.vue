8677d7e81a3ec53c36c9a57f8bb723ac41c92e42
Mike <info@mihow.net>
2020-09-21 20:05:49 +1000

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,11 +6,9 @@
     </h3>
   </div>
   <div class="w-full text-justify text-gray-700 py-6 px-4 md:text-lg">
-    <p>I’m a creative, empathic, analytical and resilient 36-year-old electronic music artist, freelance data engineer, and a former logistics officer in the Army Reserve.
-Last December I returned home, having lived abroad in Amsterdam and Kuala Lumpur for nine years. These experiences exposed me to unique and diverse lifestyles and perspectives not found in Australia. My time away from home has instilled profound gratitude for our unique foothills environment and lifestyle, and sense of community found in Ferntree Gully, which has been my family home for 20 years.
-I want to listen to the needs of the community with empathy and provide a way forward that supports the community, nurtures the environment and ensures wellbeing.</p>
-    <p class="mt-6">I decided to move back home in late 2019 when visiting Melbourne for an extended period to help my mum out during her breast cancer treatment and recovery. I came to the conclusion from living overseas and my many travels, that Ferntree Gully and the surrounding area is one of the best places to live in the world with our unique blend of modern amenities and services and connection to raw nature with the Dandenongs National Park. I’d like to think that we can keep this place special and I’d want to do my part to help ensure it stays that way for future generations.</p>
-    <p class="mt-6">I don’t know everything, but I know enough to find and listen to those that know what they are talking about, and we have many people like this in our community. I want to talk with as many people as possible in the Dobson area community, but more importantly to listen, empathise and take action on your behalf. Vote for me as the next councillor for Dobson in the City of Knox council.</p>
+    <p>I’m a creative, empathic, analytical and resilient 36-year-old electronic music artist, freelance data engineer, and a former logistics officer in the Army Reserve. Last December I returned home, having lived abroad in Amsterdam and Kuala Lumpur for nine years. These experiences exposed me to unique and diverse lifestyles and perspectives not found in Australia.</p>
+    <p class="mt-6">My time away from home has instilled profound gratitude for our unique foothills environment and lifestyle, and sense of community found in Ferntree Gully, which has been my family home for 20 years.</p>
+    <p class="mt-6">I want to listen to the needs of the community with empathy and provide a way forward that supports the community, nurtures the environment and ensures wellbeing.</p>
   </div>
 </div>
 </template>
--- src/views/Principles.vue
+++ src/views/Principles.vue
@@ -13,14 +13,15 @@
           <h2 class="text-2xl border-b-2 border-purple-500">Support the Community</h2>
           <div class="text-gray-700 text-left mt-6">
             <ul class="list-disc">
-              <li class="py-2"><strong>Freeze rates</strong> and help the financially vulnerable with fair rate discounts and deferments</li>
+              <li class="py-2"><strong>Fully Independent - No political party membership</strong></li>
+              <li class="py-2"><strong>Freeze rates in FY21/22</strong> and continue to help the financially vulnerable with fair rate discounts and deferments</li>
               <li class="py-2">Town planning and development must <strong>respect the communities wishes</strong></li>
               <li class="py-2">Create more affordable housing options by making <strong>Knox Tiny-house friendly</strong></li>
-              <li class="py-2">Increase transparency with <strong>quaterly ward meetings</strong> and adoption of Blockchain governance technology</li>
+              <li class="py-2">Increase transparency with <strong>increased community engagement</strong> and adoption of Blockchain governance technology</li>
               <li class="py-2">Stimulate the local economy by ensuring <strong>council procurement prioritises local small business</strong> and startups</li>
-              <li class="py-2"><strong>Create jobs</strong> by establishing startup incubators and grants especially for innovation involving Permaculture, High-Tech Manufacturing, Creative Design and Blockchain technologies</li>
+              <li class="py-2"><strong>Promote FTG and UFTG towns as the hiking gateways to the Dandenongs</strong> to boost town activity and reduce carpark congestion at 1000 steps</li>
+              <li class="py-2">Put our community on the map by <strong>creating a thriving arts scene</strong> with renewed murals, establishing legal walls for street art and financial and logistic support for local artist co-ops</li>
               <li class="py-2">Provide <strong>financial and logistic support</strong> for our local community non-for profits and sports clubs</li>
-              <li class="py-2">Put our community on the map by <strong>creating a thriving arts scene</strong> with renewed murals and establishing legal walls for street art.</li>
             </ul>
           </div>
         </div>
@@ -32,9 +33,9 @@
           <h2 class="text-2xl border-b-2 border-purple-500">Nurture the Environment</h2>
           <div class="text-gray-700 text-left mt-6">
             <ul class="list-disc">
-              <li class="py-2"><strong>Protecting</strong> the foothills skyline and communities connection to the Dandenong's National Park from over-development</li>
-              <li class="py-2">Make Knox a leader in Permacultur adoption by <strong>establishing a Permaculture committee</strong> and creating a Permaculture adoption roadmap in the next council plan</li>
-              <li class="py-2"><strong>Invest in resources and practices</strong> that reduce overheads to community and the environment</li>
+              <li class="py-2"><strong>Protect the foothills skyline</strong> and communities connection to the Dandenong's National Park from over-development</li>
+              <li class="py-2">Make Knox a leader in Permaculture adoption by <strong>establishing a Permaculture committee</strong> and creating a Permaculture adoption roadmap in the next council five year plan</li>
+              <li class="py-2"><strong>Rebalance engagement</strong> between council and environment advocacy groups</li>
             </ul>
           </div>
         </div>
@@ -47,8 +48,8 @@
           <div class="text-gray-700 text-left mt-6">
             <ul class="list-disc">
               <li class="py-2">Provide affordable and well resourced health and education services, with a <strong>focus on community mental health, youth outreach and high quality care of our elders</strong></li>
-              <li class="py-2"><strong>Utilise vacant properties</strong> in the community as short-term accomodation for the homeless</li>
-              <li class="py-2">Use vacant council land as sites for <strong>demountable emergency shelters</strong></li>
+              <li class="py-2">Support the use of <strong>Churches as temporary winter accommodation</strong> for the homeless</li>
+              <li class="py-2">Enable access to public <strong>shower facilities for the homeless</strong></li>
             </ul>
           </div>
         </div>
