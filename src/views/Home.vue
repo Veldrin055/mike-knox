0ffbc37becdff9af455eaa7e184137919c2eb35e
Daniel Morrison <daniel.morrison@oramon.nl>
2020-09-22 12:06:33 +1000

    add break to copyright

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="h-screen py-8 md:py-12 purple-grad bg-fixed">
   <div class="m-auto container h-full max-h-full px-6 flex flex-col md:items-center">
-    <img class="object-contain h-full m-auto md:my-8" src="/assets/img/candidate.jpg" />
+    <img class="object-contain m-auto md:my-8" src="/assets/img/candidate.jpg" style="max-height: 95%"/>
   </div>
 </div>
 </template>
