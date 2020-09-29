d2504d87334183b9a97c0d643651319999992b9d
Daniel Morrison <daniel.morrison@oramon.nl>
2020-09-29 21:28:24 +1000

    add youtube social

--- src/components/FontAwesome.js
+++ src/components/FontAwesome.js
@@ -1,9 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faInstagram, faPagelines, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faPagelines, faLinkedin, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faHeart, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export const setupFontAwesome = (vue) => {
-  library.add(faInstagram, faHeart, faUsers, faPagelines, faLinkedin, faFacebook)
+  library.add(faInstagram, faHeart, faUsers, faPagelines, faLinkedin, faFacebook, faYoutube)
   vue.component('font-awesome-icon', FontAwesomeIcon)
 }
